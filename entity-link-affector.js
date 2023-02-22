779c183c33bf8eedeacac2d5cd5c5c70dcc28121
Dimitriy <devinterx@gmail.com>
2023-02-23 02:29:35 +0300

    + add support for PF2E, refactoring to support lib-wrapper

--- entity-link-affector.js
+++ entity-link-affector.js
@@ -1,82 +1,76 @@
-// CONFIG.debug.hooks = true;
+// CONFIG.debug.hooks = false;
 
-const entityEffectLinkAffector = html => {    
-    if (html === null || html === undefined) return undefined;
+const _htmlClosest = (child, selectors) => !(child instanceof Element) ? null : child.closest(selectors);
+const _onClickContentLink = async (event) => {
+  event.preventDefault();
+  const doc = await fromUuid(event.currentTarget.dataset.uuid);
 
-    const links = html.find('a.content-link:not([data-affector])');
+  if (!(event.shiftKey || event.altKey)) return doc?._onClickDocumentLink(event);
 
-    if (links === null || links === undefined || (Array.isArray(links) && !links.length)) return undefined;
+  const target = event.currentTarget;
+  let itemSource = doc.toObject();
 
-    setTimeout(() => {
-        for (let link of links) {
-            const $link = $(link);
-            $link.attr('data-affector', '1');
-            $link.removeClass('content-link');
-            $link.addClass('content-link-affector');
+  if (CONFIG.PF2E) {
+    if (target.classList.contains('content-link')) {
+      if (itemSource.type === 'effect' || itemSource.type === 'affliction' || itemSource.type === 'condition') {
+        // Detect condition count if available
+        const name = target.innerText.trim();
+        const match = name.match(/[0-9]+/);
+        if (match && itemSource.system?.value && itemSource.system?.value?.isValued)
+          itemSource.system.value.value = Number(match[0]);
+
+        // Detect spell level of containing element, if available
+        const containerElement = _htmlClosest(target, '[data-cast-level]');
+        const castLevel = Number(containerElement?.dataset.castLevel);
+        if (castLevel > 0 && itemSource.system?.level)
+          itemSource.system.level.value = castLevel;
+
+        const messageId = _htmlClosest(target, 'li.chat-message')?.dataset.messageId;
+        const message = game.messages.get(messageId ?? '');
+        if (message?.actor) {
+          const {actor, token, target} = message;
+          const roll = message.rolls.at(-1);
+
+          itemSource.system.context = {
+            origin: {
+              actor: actor.uuid,
+              token: token?.uuid ?? null,
+              item: message.item?.uuid ?? null,
+            },
+            target: target ? {actor: target.actor.uuid, token: target.token.uuid} : null,
+            roll: roll
+              ? {
+                total: roll.total,
+                degreeOfSuccess: roll?.degreeOfSuccess ?? null,
+              }
+              : null,
+          };
         }
-    }, 0);
-};
-
-const _hasEffect = (actor, effect) => {
-  return actor.itemTypes.effect.find(e => e.flags.core?.sourceId === effect);
-}
-
-const _onClickContentEffectLink = async (event) => {
-    event.preventDefault();
-    const doc = await fromUuid(event.currentTarget.dataset.uuid);
-    const effect = doc.toObject();
-    if (event.shiftKey) {
-        const controlled = canvas?.tokens?.controlled;
-        if (controlled?.length > 0) {
-             controlled.map(async (token) => {if (!_hasEffect(token.actor, effect)) await token.actor.createEmbeddedDocuments('Item', [effect])});
-        } else ui.notifications.error(game.i18n.localize('PF2E.ErrorMessage.NoTokenSelected'))
-        window._token = controlled[0];
-    } else if (event.altKey) {
-        const targets = game?.user?.targets;
-        if (targets?.size > 0) {
-            targets.map(async (token) => {if (!_hasEffect(token.actor, effect)) await token.actor.createEmbeddedDocuments('Item', [effect])});
-        } else ui.notifications.error(game.i18n.localize('PF2E.ErrorMessage.NoTokenSelected'))
-    } else {
-        return doc?._onClickDocumentLink(event);
+      }
     }
+  }
+
+  if (event.shiftKey) {
+    const controlled = canvas?.tokens?.controlled;
+    if (controlled?.length > 0) {
+      controlled.map(async (token) => await token.actor.createEmbeddedDocuments('Item', [itemSource]));
+    } else ui.notifications.error(game.i18n.localize('PF2E.ErrorMessage.NoTokenSelected'))
+    window._token = controlled[0];
+  } else if (event.altKey) {
+    const targets = game?.user?.targets;
+    if (targets?.size > 0) {
+      targets.map(async (token) => await token.actor.createEmbeddedDocuments('Item', [itemSource]));
+    } else ui.notifications.error(game.i18n.localize('PF2E.ErrorMessage.NoTokenSelected'))
+  }
 };
-
-const _onDragContentEffectLink = (event) => {
-    event.stopPropagation();
-    const a = event.currentTarget;
-    let dragData = null;
-
-    // Case 1 - Compendium Link
-    if ( a.dataset.pack ) {
-        const pack = game.packs.get(a.dataset.pack);
-        let id = a.dataset.id;
-        if ( a.dataset.lookup && pack.index.size ) {
-            const entry = pack.index.find(i => (i._id === a.dataset.lookup) || (i.name === a.dataset.lookup));
-            if ( entry ) id = entry._id;
-        }
-        if ( !a.dataset.uuid && !id ) return false;
-        const uuid = a.dataset.uuid || `Compendium.${pack.collection}.${id}`;
-        dragData = { type: pack.documentName, uuid };
-    }
-
-    // Case 2 - World Document Link
-    else {
-        const doc = fromUuidSync(a.dataset.uuid);
-        dragData = doc.toDragData();
-    }
-
-    event.originalEvent.dataTransfer.setData("text/plain", JSON.stringify(dragData));
-};
-  
-const body = $("body");
-body.on("click", "a.content-link-affector", _onClickContentEffectLink);
-body.on("dragstart", "a.content-link-affector", _onDragContentEffectLink);
 
 Hooks.on('init', () => {
-    console.info('Entity Link Affector enabled');
-    Hooks.on('renderChatMessage', (_, html) => entityEffectLinkAffector(html));
-    Hooks.on('renderChatLog', (_, html) => entityEffectLinkAffector(html));
-    Hooks.on('renderChatPopout', (_, html) => entityEffectLinkAffector(html));
+  console.info('Entity Link Affector enabled' + (CONFIG.PF2E ? ' (PF2E support)' : ''));
+  if (typeof libWrapper === 'function') {
+    libWrapper.register('entity-link-affector', 'TextEditor._onClickContentLink', function (wrapped, ...args) {
+      TextEditor._onClickContentLink = _onClickContentLink;
+    }, 'OVERRIDE');
+  }
 });
 
 
